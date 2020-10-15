ae0a8290ca95a48949dffc2e7694630be5575576
Duane Johnson <duane.johnson@gmail.com>
2020-10-15 16:25:51 -0600

    CSS & rollup - support CSS can have relative url() references - support global CSS vars - add prelim support for rollup bundling

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,14 +1,43 @@
 module.exports = {
   mount: {
-    public: '/',
-    src: '/_dist_',
+    public: "/",
+    src: "/_dist_",
+  },
+  devOptions: {
+    output: "stream",
+    open: "none",
   },
   plugins: [
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-dotenv',
+    "@snowpack/plugin-svelte",
+    "snowpack-plugin-relative-css-urls",
+    "@snowpack/plugin-dotenv",
     [
-      '@snowpack/plugin-run-script',
-      {cmd: 'svelte-check --output human', watch: '$1 --watch', output: 'stream'},
+      "@snowpack/plugin-run-script",
+      {
+        cmd: "svelte-check --output human",
+        watch: "$1 --watch",
+        output: "stream",
+      },
+    ],
+    [
+      "snowpack-plugin-rollup-bundle",
+      {
+        // equivalent to inputOptions.input from Rollup
+        entrypoints: "build/_dist_/index.js",
+
+        extendConfig: (config) => {
+          // https://rollupjs.org/guide/en/#outputoptions-object
+          config.outputOptions = {
+            // Prefer rollup output goes to a ./dist folder instead of re-writing ./build
+            dir: "dist",
+          };
+
+          // https://rollupjs.org/guide/en/#inputoptions-object
+          // config.inputOptions = {};
+
+          return config;
+        },
+      },
     ],
   ],
 };
---
+++ src/ui/Button/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<style>
+  div {
+    padding: 0;
+    width: var(--size);
+    height: var(--size);
+    background-size: var(--size) var(--size);
+    background-image: url(./checkmark.png);
+  }
+</style>
+
+<h1>Checkmark:</h1>
+<div />
